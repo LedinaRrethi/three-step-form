a3ef129f883ab0308991e3bc538db68f505f4e85
Ledina Rrethi <ledinarrethi@gmail.com>
2024-10-30 10:39:11 +0100

    1. Refactored useMultiStepForm: Replaced useState with useEffect to initialize formData and step from localStorage. This change improves initial data loading and ensures correct defaults when localStorage is empty or unavailable.
    2. Remove items from local storage on submit

--- src/hooks/useMultipleStepForm.jsx
+++ src/hooks/useMultipleStepForm.jsx
@@ -8,16 +8,30 @@ const useMultiStepForm = () => {
     step3Data: { companyName: '', companyCode: '' },
   };
 
-  const savedData = JSON.parse(localStorage.getItem('formData'));
-  const savedStep = JSON.parse(localStorage.getItem('step'))
+  //better approach to use useEffect
+  // const savedData = JSON.parse(localStorage.getItem('formData'));
+  // const savedStep = JSON.parse(localStorage.getItem('step'))
 
-  const [formData, setFormData] = useState(savedData || initialFormData);
-  const [step , setStep] = useState(savedStep || 1);
+  // const [formData, setFormData] = useState(savedData || initialFormData);
+  // const [step , setStep] = useState(savedStep || 1);
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [step, setStep] = useState(1);
 
   useEffect(() => {
-    localStorage.setItem('formData', JSON.stringify(formData));
-    localStorage.setItem('step', JSON.stringify(step));
-  }, [formData, step]);
+    const savedData = JSON.parse(localStorage.getItem('formData')) || initialFormData;
+    const savedStep = JSON.parse(localStorage.getItem('step')) || 1;
+    if (savedData) setFormData(savedData);
+    if (savedStep) setStep(savedStep);
+
+    console.log(savedData);
+  }, []);
+
+
+  // useEffect(() => {
+  //   localStorage.setItem('formData', JSON.stringify(formData));
+  //   localStorage.setItem('step', JSON.stringify(step));
+  // }, [formData, step]);
 
   const updateFormData = (stepKey, field, value) => {
     setFormData((prev) => ({
@@ -27,11 +41,16 @@ const useMultiStepForm = () => {
         [field]: value,
       },
     }));
+
+    localStorage.setItem('formData', JSON.stringify(formData));
+    localStorage.setItem('step', JSON.stringify(step));
   };
 
   const resetFormData = () => {
     setFormData(initialFormData)
-    setStep(1)
+    setStep(1);
+    localStorage.removeItem('formData');
+    localStorage.removeItem('step');
   };
 
   const nextStep = () => setStep((prev) => (prev < 3 ? prev + 1 : prev));
